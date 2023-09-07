dd05fdb628631568c56972186d8f9df80a2b19ad
naeem <naeemislamjr182@gmail.com>
2023-09-08 04:52:28 +0600

    react-six-core-concept-recap

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  const olstyle = {
+    border: "2px solid purple",
+
+
+    margin: "10px",
+    padding: "5px",
+    borderRadius: "10px"
+  }
+
+  return (
+    <>
+
+      <h2>react-six-core-concept-recap</h2>
+      <div style={olstyle}>
+        <ol >
+          <li>Components</li>
+          <li>JSX</li>
+          <li>Props</li>
+          <li>Event Handler</li>
+          <li>State</li>
+          <li>load data</li>
+        </ol>
+
+      </div>
+    </>
+  )
+}
+
+export default App
